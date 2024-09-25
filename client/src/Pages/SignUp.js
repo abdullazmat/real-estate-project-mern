7cd21c70e93c53eb463bf16ff12e3bc281a5a591
Abdullah <143247953+abdullazmat@users.noreply.github.com>
2024-09-25 16:59:17 +0500

    Completed Sign Up Page

--- client/src/Pages/SignUp.js
+++ client/src/Pages/SignUp.js
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom"
+
 function SignUp() {
     return (
-        <div>
-            
+        <>
+        <h1 className = "container my-5 ms-auto text-center fw-bold">Sign Up</h1>
+        <form className = "container my-5 signup-form">
+        <div className="mb-3">
+            <label for="username" className="form-label" >username</label>
+            <input type="text" className="form-control" id="username" autocomplete="off"  required placeholder = "abdullah"/>
         </div>
+        <div className="mb-3">
+            <label for="password" className="form-label">Email</label>
+            <input type="email" className="form-control" id="password" required placeholder = "abc@gmail.com"/>
+        </div>
+        <div className="mb-3">
+            <label for="password" className="form-label">Password</label>
+            <input type="password" className="form-control" required id="password"/>
+        </div>
+        <div class="d-grid my-4 col-12 ">
+        <button class="btn signup-btn rounded-pill" type="button">Sign Up </button>
+        <p  class="form-text my-3">Already have an account?</p> <Link to={"/sign-in"}>Sign In</Link>
+        </div>        
+</form>
+
+</>
     )
 }
 
