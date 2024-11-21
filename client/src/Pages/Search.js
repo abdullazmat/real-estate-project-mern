3004f343606df0c7aabe3e80dadb7056d54ec2c8
Abdullah <143247953+abdullazmat@users.noreply.github.com>
2024-11-21 13:19:15 +0500

    Added OnChange and OnSubmit functionality into Search Page

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,6 +1,106 @@
-import React from "react";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 function Search() {
+  const navigate = useNavigate();
+  const [sidebardata, setSidebarData] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+  console.log(listings);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const parkingFromUrl = urlParams.get("parking");
+    const furnishedFromUrl = urlParams.get("furnished");
+    const offerFromUrl = urlParams.get("offer");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (
+      searchTermFromUrl ||
+      typeFromUrl ||
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    )
+      setSidebarData({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        parking: parkingFromUrl === "true" ? true : false,
+        furnished: furnishedFromUrl === "true" ? true : false,
+        offer: offerFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+
+    const fetchListings = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      setListings(data);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "rent" ||
+      e.target.id === "sale"
+    ) {
+      setSidebarData({ ...sidebardata, type: e.target.id });
+    }
+    if (e.target.id === "searchTerm") {
+      setSidebarData({ ...sidebardata, searchTerm: e.target.value });
+    }
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setSidebarData({
+        ...sidebardata,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+      const order = e.target.value.split("_")[1] || "desc";
+      setSidebarData({ ...sidebardata, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
+
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <div className="container-fluid p-0">
       {/* Row for Form and Right Section */}
@@ -10,7 +110,7 @@ function Search() {
           className="col-12 col-md-4 py-4 px-3 ms-3 border-end h-auto h-md-100 min-vh-md-100"
           style={{ borderColor: "black", borderWidth: "2px" }}
         >
-          <form>
+          <form onSubmit={handleSubmit}>
             {/* Search Term */}
             <div className="d-flex align-items-center mb-4">
               <b>
@@ -21,6 +121,8 @@ function Search() {
                 className="form-control w-50 ms-2"
                 placeholder="Term"
                 id="searchTerm"
+                value={sidebardata.searchTerm}
+                onChange={handleChange}
               />
             </div>
 
@@ -34,9 +136,11 @@ function Search() {
                   <input
                     type="checkbox"
                     className="form-check-input"
-                    id="rent-sale"
+                    id="all"
+                    onChange={handleChange}
+                    checked={sidebardata.type === "all"}
                   />
-                  <label className="form-check-label" htmlFor="rent-sale">
+                  <label className="form-check-label" htmlFor="all">
                     Rent & Sale
                   </label>
                 </div>
@@ -45,6 +149,8 @@ function Search() {
                     type="checkbox"
                     className="form-check-input"
                     id="rent"
+                    onChange={handleChange}
+                    checked={sidebardata.type === "rent"}
                   />
                   <label className="form-check-label" htmlFor="rent">
                     Rent
@@ -55,6 +161,8 @@ function Search() {
                     type="checkbox"
                     className="form-check-input"
                     id="sale"
+                    onChange={handleChange}
+                    checked={sidebardata.type === "sale"}
                   />
                   <label className="form-check-label" htmlFor="sale">
                     Sale
@@ -65,6 +173,8 @@ function Search() {
                     type="checkbox"
                     className="form-check-input"
                     id="offer"
+                    onChange={handleChange}
+                    checked={sidebardata.offer}
                   />
                   <label className="form-check-label" htmlFor="offer">
                     Offer
@@ -84,6 +194,8 @@ function Search() {
                     type="checkbox"
                     className="form-check-input"
                     id="parking"
+                    onChange={handleChange}
+                    checked={sidebardata.parking}
                   />
                   <label className="form-check-label" htmlFor="parking">
                     Parking
@@ -94,6 +206,8 @@ function Search() {
                     type="checkbox"
                     className="form-check-input"
                     id="furnished"
+                    onChange={handleChange}
+                    checked={sidebardata.furnished}
                   />
                   <label className="form-check-label" htmlFor="furnished">
                     Furnished
@@ -107,11 +221,18 @@ function Search() {
               <b>
                 <p className="m-0 mb-2">Sort:</p>
               </b>
-              <select className="form-select w-50 ms-2 " id="sort-order">
-                <option selected>Price High To Low</option>
-                <option>Price Low To High</option>
-                <option>Latest</option>
-                <option>Oldest</option>
+              <select
+                className="form-select w-50 ms-2 "
+                id="sort_order"
+                onChange={handleChange}
+                defaultValue={"created_at_desc"}
+              >
+                <option value="regularPrice_desc" selected>
+                  Price High To Low
+                </option>
+                <option value="regularPrice_asc">Price Low To High</option>
+                <option value="createdAt_desc">Latest</option>
+                <option value="createdAt_asc">Oldest</option>
               </select>
             </div>
 
